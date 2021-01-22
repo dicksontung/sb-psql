f2c7757c6e0ad8960046e949681c93addc371c18
dickson <dtung@moneylion.com>
2021-01-22 15:55:26 +0800

    Add README

---
+++ README.md
@@ -0,0 +1,8 @@
+# How to run
+```
+mvn spring-boot:run
+```
+# How to test
+Open `test.http` using IntelliJ Idea http client
+
+https://www.jetbrains.com/help/idea/http-client-in-product-code-editor.html
--- src/main/java/com/moneylion/PersonRepository.java
+++ src/main/java/com/moneylion/PersonRepository.java
@@ -1,6 +1,5 @@
 package com.moneylion;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
